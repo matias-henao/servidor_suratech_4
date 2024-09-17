2a6f5b7faad8b04b38e875a01774c34773650a2a
matias-henao <mdesarrollador03@gmail.com>
2024-09-17 15:08:31 -0500

    actualizacion

---
+++ src/main/java/com/example/servidor_suratech_4/MODELOS/Paciente.java
@@ -0,0 +1,16 @@
+package com.example.servidor_suratech_4.MODELOS;
+
+import java.time.LocalDate;
+
+public class Paciente {
+    private Integer id;
+    private String nombres;
+    private Integer edad;
+    private LocalDate fechaNacimiento;
+    private String documento;
+    private String telefono;
+    private Integer copago;
+    private String email;
+    private String direccion;
+    private String ips;
+}
